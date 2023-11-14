73c100bc4bc7272f14594bdb5c8b278d42ae2f9a
Evan <eljohn1014@gmail.com>
2023-11-13 21:23:13 -0500

    Fix local_link separator

--- network.go
+++ network.go
@@ -19,7 +19,7 @@ type IPv4Data struct {
 
 type IPv6Data struct {
 	SLAAC        netip.Prefix   `json:"slaac"`
-	LinkLocal    netip.Prefix   `json:"link-local"`
+	LinkLocal    netip.Prefix   `json:"link_local"`
 	Ranges       []netip.Prefix `json:"ranges"`
 	SharedRanges []netip.Prefix `json:"shared-ranges"`
 }
