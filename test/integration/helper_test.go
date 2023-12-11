2cea382ff5093ecd1799175640b5a7cccdd7c9d9
Lena Garber <lgarber@akamai.com>
2023-12-11 12:46:38 -0500

    oops

--- test/integration/helper_test.go
+++ test/integration/helper_test.go
@@ -1,6 +1,8 @@
 package integration
 
 import (
+	"context"
+	"log"
 	"os"
 
 	"github.com/linode/go-metadata"
@@ -12,20 +14,20 @@ var metadataClient *metadata.Client
 var linodeClient *linodego.Client
 
 func init() {
-	//if testToken == "" {
-	//	log.Fatal("LINODE_TOKEN must be specified to run the E2E test suite")
-	//}
-	//
-	//mdsClient, err := metadata.NewClient(
-	//	context.Background(),
-	//)
-	//if err != nil {
-	//	log.Fatalf("failed to create client: %s", err)
-	//}
-	//
-	//apiClient := linodego.NewClient(nil)
-	//apiClient.SetToken(testToken)
-	//
-	//metadataClient = mdsClient
-	//linodeClient = &apiClient
+	if testToken == "" {
+		log.Fatal("LINODE_TOKEN must be specified to run the E2E test suite")
+	}
+
+	mdsClient, err := metadata.NewClient(
+		context.Background(),
+	)
+	if err != nil {
+		log.Fatalf("failed to create client: %s", err)
+	}
+
+	apiClient := linodego.NewClient(nil)
+	apiClient.SetToken(testToken)
+
+	metadataClient = mdsClient
+	linodeClient = &apiClient
 }
