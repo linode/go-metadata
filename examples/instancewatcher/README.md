68e7efa128010e8faa28c5fa1def11d9af79d721
Lena Garber <114949949+lgarber-akamai@users.noreply.github.com>
2023-12-12 11:30:34 -0500

    ref: Refactor watcher system to use genericWatcher; improve project documentation (#20)
    * Refactor watcher system to use genericWatcher; improve docs
    
    * oops
    
    * Add examples to package docs

---
+++ examples/instancewatcher/README.md
@@ -0,0 +1,11 @@
+# Instance Watcher Example
+
+This example makes use of the InstanceWatcher struct to watch for changes to the current Linode instance.
+
+### Running this Example
+
+NOTE: This example must be run from within a Linode instance.
+
+```bash
+go run main.go
+```
---
+++ examples/networkwatcher/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	metadata "github.com/linode/go-metadata"
+	"log"
+	"time"
+)
+
+func main() {
+	ctx := context.Background()
+
+	// Create a new client
+	client, err := metadata.NewClient(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create a new network watcher
+	networkWatcher := client.NewNetworkWatcher(
+		metadata.WatcherWithInterval(10 * time.Second),
+	)
+
+	// Start the network watcher in a goroutine.
+	go networkWatcher.Start(ctx)
+
+	// Wait for changes
+	for {
+		select {
+		case data := <-networkWatcher.Updates:
+			log.Printf(
+				"Change to network configuration detected.\nNew data: %v\n",
+				data,
+			)
+		case err := <-networkWatcher.Errors:
+			log.Fatalf("Got error from network watcher: %s", err)
+		}
+	}
+}
