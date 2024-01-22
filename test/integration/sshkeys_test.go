9093622df99a8d8598ebd43f63a91df7d1895fb7
Lena Garber <114949949+lgarber-akamai@users.noreply.github.com>
2024-01-22 10:22:01 -0500

    ref: (BREAKING) Use map for Client.GetSSHKeys(...) response (#28)
    * Use map for Client.GetSSHKeys(...) response
    
    * Add optional skip
    
    * Update test/integration/sshkeys_test.go
    
    Co-authored-by: Ye Chen <127243817+yec-akamai@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Ye Chen <127243817+yec-akamai@users.noreply.github.com>

---
+++ test/integration/sshkeys_test.go
@@ -0,0 +1,24 @@
+package integration
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetSSHKeys(t *testing.T) {
+	t.Parallel()
+
+	sshKeys, err := metadataClient.GetSSHKeys(context.Background())
+	assert.NoError(t, err)
+
+	if len(sshKeys.Users) < 1 {
+		t.Skip(
+			"The current instance does not have any SSH keys configured, skipping...")
+	}
+
+	for _, v := range sshKeys.Users {
+		assert.Greater(t, len(v), 0)
+	}
+}
